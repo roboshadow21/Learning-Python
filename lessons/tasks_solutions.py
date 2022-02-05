a216110d42a4561bd560d6a32a6b3941237603ad
Alex <roboshadow@mail.ru>
2022-02-05 20:22:47 +0300

    task3-4

--- lessons/tasks_solutions.py
+++ lessons/tasks_solutions.py
@@ -20,3 +20,15 @@ for i in range(len(lst) - 1):
         maximum = lst[i + 1]
 print(maximum)
 
+# 3. Вывести на экран числа от -N до N
+
+numbers = range(-10, 11)
+for i in numbers:
+    print(i, end=' ')
+
+# 4. Показать первую цифру дробной части числа
+print()
+x = 32.86793
+y = int(x)
+z = x - y
+print(int(z * 10))
