a9cb0e084012d27228cc5271ab5bc6fe88498f9e
Alex <roboshadow@mail.ru>
2022-03-20 13:06:14 +0300

    update user_input.py

--- lessons/data_base/user_input.py
+++ lessons/data_base/user_input.py
@@ -1,10 +1,59 @@
 from manage import *
 
-user = input('Enter num: ')
-if user == '1':
-    show_all_employees_contacts()
-elif user == '2':
-    show_all_stuff_salary()
+change = {
+    '1': add_new_employee,
+    '2': add_new_position,
+    '3': change_status,
+    '4': add_new_contacts,
+    '5': change_contacts,
+    '6': add_new_address
+}
+show = {
+    '1': show_contacts,
+    '2': show_addresses,
+    '3': show_all_stuff_salary,
+    '4': show_employee_salary,
+    '5': show_all_employees_contacts,
+    '6': find_employee_contacts,
+    '7': count_stuff,
+    '8': show_employee_age,
+    '9': show_status
+}
 
+
+def user():
+    while True:
+        data = input('Продолжить работу? Y/N: ').lower()
+        if data == 'n':
+            break
+        elif data == 'y':
+            data = input('Выберите "1" для изменения данных, "2" для получения данных: ')
+            if data == '1':
+                query = input(
+                    'Выберите:\n'
+                    '1 - Добавить нового сотрудника\n'
+                    '2 - Добавить новую должность\n'
+                    '3 - Изменить статус сотрудника\n'
+                    '4 - Добавить новые контактные данные\n'
+                    '5 - Изменить контактные данные\n'
+                    '6 - Добавить новый адрес сотрудника\n: '
+                )
+                change[query]()
+            elif data == '2':
+                query = input(
+                    'Выберите:\n'
+                    '1 - Показать контакты сотрудника\n'
+                    '2 - Показать адреса сотрудников\n'
+                    '3 - Показать зарплату всего персонала\n'
+                    '4 - Показать зарплату сотрудника\n'
+                    '5 - Показать контакты всех сотрудников\n'
+                    '6 - Показать должность и контакты сотрудника\n'
+                    '7 - Показать количество сотрудников\n'
+                    '8 - Показать возраст сотрудников\n'
+                    '9 - Показать статус\n: ')
+                show[query]()
+
+
+user()
 
 conn.close()
