c032bbfe9e81c8143803d83171896299f33070e1
Alex <roboshadow@mail.ru>
2022-03-20 21:45:53 +0300

    update t_bot

--- lessons/t_bot/commands.py
+++ lessons/t_bot/commands.py
@@ -1,6 +1,29 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+from datetime import datetime
 
 
 def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    update.message.reply_text(f'Hello {update.effective_user.first_name}, {update.effective_user.id}')
+
+
+def user_id(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Привет {update.effective_user.first_name} {update.effective_user.last_name}!')
+
+
+def summa(update: Update, context: CallbackContext) -> None:
+    msg = update.message.text.split()
+    update.message.reply_text(f'Сумма = {int(msg[1]) + int(msg[2])}')
+
+
+def time(update: Update, context: CallbackContext) -> None:
+    current_time = datetime.now().time()
+    update.message.reply_text(f'Сейчас {current_time}')
+
+
+def bad(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Ибать вас некому!')
+
+
+def test(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'{update.message.text}')
--- lessons/t_bot/start.py
+++ lessons/t_bot/start.py
@@ -2,13 +2,15 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from commands import *
 
-# def hello(update: Update, context: CallbackContext) -> None:
-#     update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
 
 updater = Updater('5159704813:AAHhPWKp9RbyGk4a4F5SEhwphAPiwxGXfvk')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('hi', user_id))
+updater.dispatcher.add_handler(CommandHandler('summa', summa))
+updater.dispatcher.add_handler(CommandHandler('time', time))
+updater.dispatcher.add_handler(CommandHandler('bad', bad))
+updater.dispatcher.add_handler(CommandHandler('test', test))
 
 updater.start_polling()
 updater.idle()
